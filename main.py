22911deaa11a3dffa9ad91c1bc86f1b218c561f3
eitanadmoni <117032277+eitanadmoni@users.noreply.github.com>
2025-02-11 11:07:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def main():
     n = input()
-    for i in range(1, n + 1):
-      if i % 8 == 0 or str(8) in str(i):
-        print("ARAZIM")
-      else:
-        print(i)
+    for i in range(1, int(n) + 1):
+        if i % 8 == 0 or str(8) in str(i):
+            print("ARAZIM")
+        else:
+            print(i)
     return
 
-if __name__ == "__main()__":
-      main()
+
+if __name__ == "__main__":
+    main()
